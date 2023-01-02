95a9bcfa0526fcc4dc804b407a4f2a02a381cf60
Ibwedagama <wardana.wedagama@gmail.com>
2023-01-02 15:44:00 +0800

    feat: hero section

---
+++ components/Base/Button/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="flex min-h-[26px] w-fit items-center justify-center rounded-full bg-black py-2 px-7 text-xs text-white md:min-h-[52px] md:px-8 md:text-base"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineComponent({
+  name: "BaseButton",
+});
+</script>
---
+++ components/TheHero/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <section
+    class="grid min-h-[330px] w-full grid-cols-2 px-4 pt-6 md:min-h-[766px] xl:px-0"
+  >
+    <div
+      class="relative -mr-[2rem] flex flex-col justify-center py-5 md:-mr-[4rem] xl:mr-0"
+    >
+      <h1
+        class="mb-4 font-lora text-2xl font-bold md:mb-12 md:text-7xl md:leading-tight"
+      >
+        Best house plants varieties.
+      </h1>
+      <div class="bg-white p-2">
+        <h2 class="mb-3 text-sm font-semibold md:mb-6 md:text-2xl">
+          Beautiful living greenery for homes and offices
+        </h2>
+        <p class="mb-2.5 text-xs md:mb-4 md:text-base">
+          Wer've been mentioned in the press
+        </p>
+        <BaseButton>Shop Now</BaseButton>
+        <div class="mt-6 flex w-fit md:mt-14">
+          <img
+            src="/image/Bloomberg.svg"
+            alt=""
+            width="85"
+            height="20"
+            class="pt-1 md:h-10 md:w-full"
+          />
+          <img
+            src="/image/Forbes.svg"
+            alt=""
+            width="50"
+            height="20"
+            class="ml-4 md:ml-20 md:h-8 md:w-full"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="-mr-[1rem]">
+      <picture>
+        <source
+          media="(min-width:768px)"
+          srcset="/image/hero-image-desktop.webp"
+        />
+        <source
+          media="(min-width:320px)"
+          srcset="/image/hero-image-mobile.webp"
+        />
+        <img
+          src="/image/hero-image.png"
+          alt="two green plant with wood pots"
+          height="284"
+          width="376"
+          class="h-full w-full object-cover"
+        />
+      </picture>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+defineComponent({
+  name: "TheHero",
+});
+</script>
