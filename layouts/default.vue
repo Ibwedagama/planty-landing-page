6f6b81d25f38c535d8b2ac23cab26d246fdd8fc3
Ibwedagama <wardana.wedagama@gmail.com>
2023-01-03 13:10:13 +0800

    refactor (layout): fix semantic html

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,11 @@
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content">
       <TheHeader />
-      <slot></slot>
+      <slot />
       <TheFooter />
     </div>
-    <div class="drawer-side">
-      <label for="my-drawer" class="drawer-overlay"></label>
+    <aside class="drawer-side">
+      <label for="my-drawer" class="drawer-overlay" />
       <ul class="menu w-80 bg-base-100 p-4 text-base-content">
         <li v-for="navigation in navigationLinks" :key="navigation.id">
           <NuxtLink :to="navigation.link">
@@ -15,7 +15,7 @@
           </NuxtLink>
         </li>
       </ul>
-    </div>
+    </aside>
   </div>
 </template>
 
